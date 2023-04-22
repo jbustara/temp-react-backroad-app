006769c9ca1092be8c1eb5fde9ca6aca39867445
Juan Camilo Bustamante Ramirez <juan.bustamanter@gmail.com>
2023-04-22 09:18:44 -0500

    Create react components

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import SocialLink from "./SocialLink";
+import { pageLinks, socialLinks } from "../data";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <li key={id}>
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+            return (
+              <SocialLink key={link.id} {...link} itemClass='footer-icon'/>
+            );
+          })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/Tours.js
@@ -0,0 +1,21 @@
+import Title from "./Title";
+import Tour from "./Tour";
+import { tours } from "../data";
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <div className="section-title">
+        <Title title="featured" subtitle="services" />
+      </div>
+
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return (
+            <Tour key={tour.id} {...tour}/>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Tours;
